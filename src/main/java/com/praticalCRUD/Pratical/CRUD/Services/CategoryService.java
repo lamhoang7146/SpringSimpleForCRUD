f6175ac77fbf65eba2a4f6e6f5042a5df1be1842
unknown <aztenderio7146@gmail.com>
2025-09-01 21:35:07 +0700

    feat: implement controller for category and product

--- src/main/java/com/praticalCRUD/Pratical/CRUD/Services/CategoryService.java
+++ src/main/java/com/praticalCRUD/Pratical/CRUD/Services/CategoryService.java
@@ -1,7 +1,14 @@
 package com.praticalCRUD.Pratical.CRUD.Services;
 
+import com.praticalCRUD.Pratical.CRUD.Dtos.ApiResponseDto;
+import com.praticalCRUD.Pratical.CRUD.Models.Category;
 import com.praticalCRUD.Pratical.CRUD.Repositories.CategoryRepository;
+import com.praticalCRUD.Pratical.CRUD.Ultils.ResponseHelper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -9,6 +16,10 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public ResponseEntity<ApiResponseDto<List<Category>>> getCategories() {
+        return ResponseHelper.success(this.categoryRepository.findAll(), "Get categories successfully!", HttpStatus.OK);
     }
 
 }
